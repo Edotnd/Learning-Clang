84b23802851f682a044e8284833e6df903b11206
Edotnd <edotnd@gmail.com>
2020-09-05 01:47:39 +0900

    ,

---
+++ 07/Q1/Q-01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int num;
+    scanf("%d", &num);
+
+    while (num>0)
+    {
+        printf("Hello world! \n");
+        num--;
+    }
+    
+    return 0;
+}
