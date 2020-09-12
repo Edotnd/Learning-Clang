614d1e4ef987ccfdc34dd1d3bae6690a6e48a746
Edotnd <edotnd@gmail.com>
2020-09-12 18:26:01 +0900

    .

---
+++ 11/02/StartEndString.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    char str[50]="I like C programming(?)";
+    printf("String: %s \n", str);
+
+    str[8]='\0';
+    printf("String: %s \n", str);
+
+    str[6]='\0';
+    printf("String: %s \n", str);
+    return 0;
+}
