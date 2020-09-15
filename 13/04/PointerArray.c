d9267f2178477972fe279603917e12e5ca9c0546
Edotnd <edotnd@gmail.com>
2020-09-15 19:46:37 +0900

    .

---
+++ 13/04/PointerArray.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    int num1=10, num2=20, num3=30;
+    int * arr[3]={&num1, &num2, &num3};
+
+    for(int i=0; i<3; i++)
+        printf("%d \n", *arr[i]);
+    return 0;
+}
