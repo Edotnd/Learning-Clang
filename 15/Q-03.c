100431e19c6297c97c867a5e2d0272b3243b13a8
Edotnd <edotnd@gmail.com>
2020-10-03 12:42:43 +0900

    .

---
+++ 15/Q-03.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int Odd(int *, int); // 홀수
+int Even(int *, int); // 짝수
+
+int main(void){
+    int arr[10], size_info;
+    for(int i=0; i<10; i++)
+        printf("입력: "), scanf("%d", (arr+i));
+    size_info = sizeof(arr)/sizeof(int);
+    Odd(arr, size_info), Even(arr, size_info);
+    return 0;
+}
+
+int Odd(int * arr, int size){
+    for(int i=0; i<size; i++)
+        if(*(arr+i)%2==1 || *(arr+i)==1)
+            printf("%d ", *(arr+i));
+}
+
+int Even(int * arr, int size){
+    for(int i=size-1; i>-1; i--)
+        if(*(arr+i)%2==0)
+            printf("%d ", *(arr+i));
+}
